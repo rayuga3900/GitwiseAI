07e4356b9d137e54dddf789f01bbf4cfd2e98709
rayuga3900 <159263407+rayuga3900@users.noreply.github.com>
2026-04-09 18:43:12 +0530

    Update reranker.py

--- gitwise/core/reranker.py
+++ gitwise/core/reranker.py
@@ -10,7 +10,7 @@ class Reranker:
     Scores (query, document) pairs and returns the best ranked documents.
     """
     #    BAAI/bge-reranker-base -> old reranker replaced cause of memory issue 
-    def __init__(self, model_name: str = "cross-encoder/ms-marco-MiniLM-L-6-v2", top_k: int = 20):
+    def __init__(self, model_name: str = "cross-encoder/ms-marco-MiniLM-L-6-v2", top_k: int = 10):
         self.logger = logging.getLogger(self.__class__.__name__)
 
         self.model_name = model_name
